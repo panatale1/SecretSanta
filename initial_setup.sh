ccb7121f902108e8cc71d03b2c18c37e90d31b0c
panatale1 <panatale1@gmail.com>
2016-10-26 19:46:08 -0400

    initial commit for setup and requirements

---
+++ initial_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ $(id -u) -ne 0 ]]; then
+    echo "Please run as root"
+    exit 1
+fi
+if [ $(which python) == "" ]; then
+    apt-get install python
+else
+    echo "Python already installed"
+fi
+pip = $(which pip)
+if [[ "$?" -ne 0 ]]; then
+    apt-get install -u python-pip
+fi
+pip install virtualenv
+pip install virtualenvwrapper
+
+mkdir -p ~/.virtualenvs
+
+if [[ $(grep WORKON_HOME ~/.bashrc) -ne 0 ]]; then
+    echo "export WORKON_HOME=~/.virtualenvs" >> ~/.bashrc
+fi
+
---
+++ local_setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+VENV_NAME="django-ops"
+VENV_DIR="$HOME/.virtualenvs"
+
+mkdir -p $VENV_DIR
+
+if [[ $(grep WORKON_HOME ~/.bashrc) == "" ]]; then
+    echo "export WORKON_HOME=~/.virtualenvs" >> ~/.bashrc
+fi
+
+wrapper_path=$(which virtualenvwrapper.sh)
+if [[ wrapper_path != "" ]]; then
+    echo $wrapper_path
+    if [[ $(grep "source $wrapper_path" ~/.bashrc) == "" ]]; then
+        echo "source $wrapper_path" >> ~/.bashrc
+        source "$wrapper_path"
+    fi 
+fi
+
+source $VENV_DIR/$VENV_NAME/bin/activate
+#echo $VIRTUAL_ENV
+sudo pip install --upgrade -r requirements
+django-admin startproject mysite
+cd mysite
+./manage.py runserver
